96e88cb08bc9515a3eb52961f4d1fb2f5047c533
Winter.Vfz <lisemdarenne@gmail.com>
2026-03-15 16:02:10 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import connectToWhatsapp from './Yorha/crew.js'
-import handleIncomingMessage from './events/messageHandler.js'
-
-(async() => {
-    await connectToWhatsapp(handleIncomingMessage)
-        console.log('established !')
-})()
